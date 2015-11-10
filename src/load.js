c8514175226b5c2e8ce4c3317f7d5b8650a83341
wamei <wamei.cho@gmail.com>
2015-11-10 12:55:16 +0900

    Fix load on english environment

--- src/load.js
+++ src/load.js
@@ -1,7 +1,7 @@
 import $ from 'jquery-o';
 
 $(function () {
-    let chatwork = $("script[src*='chatwork_all_ja.min.js']");
+    let chatwork = $("script[src*='chatwork_all_']");
     chatwork.after(
         $('<script>')
             .attr('src', chrome.extension.getURL('built/bundle.js'))

71a4767503e488d0e6542d128072d9c3e73a6717
wamei <wamei.cho@gmail.com>
2016-12-22 14:40:33 +0900

    fix scroll position

--- src/feature/filter.js
+++ src/feature/filter.js
@@ -3,14 +3,28 @@ import * as Settings from '../feature/settings.js';
 
 export function mention(rid) {
     let timeline = $('#_timeLine');
+    let dateHead = timeline.find('.dateHead');
+    dateHead.css('z-index', '2');
+    dateHead.css('margin', '0');
+    dateHead.css('padding-bottom', '10px');
+    let bottom = timeline.find('#_messageIdEnd');
     let target = timeline.find('.chatTimeLineMessage').not('.chatTimeLineMessageMention');
-    if (!Settings.contain('w-filter-mention-list', rid - 0)) {
-        target.each(function(){
-            $(this).children().css('display', '');
-        });
-        return;
+    let offset = timeline.offset();
+    let element = $(document.elementFromPoint(offset.left + 10, offset.top + 1));
+    if (!element.hasClass('chatTimeLineMessage') && !element.hasClass('dateHead')) {
+        element = element.parent();
     }
-    target.each(function(){
-        $(this).children().css('display', 'none');
-    });
+
+    let is_bottom = bottom.position().top < timeline.innerHeight();
+
+    if (!Settings.contain('w-filter-mention-list', rid - 0)) {
+        target.children().css('display', '');
+    } else {
+        target.children().css('display', 'none');
+    }
+    if (is_bottom) {
+        bottom[0].scrollIntoView();
+    } else {
+        element[0].scrollIntoView();
+    }
 }

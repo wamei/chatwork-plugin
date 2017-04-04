b752bd7538c10fb26ac0cd51166ce0192585f075
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2017-04-04 11:28:03 +0900

    highlight block when it is displayed

--- src/feature/highlight.js
+++ src/feature/highlight.js
@@ -2,12 +2,26 @@ import $ from 'jquery';
 import hljs from 'highlight.js';
 import * as Settings from '../feature/settings.js';
 
+let observedTargets = [];
+let observer = new IntersectionObserver(function (changes) {
+    for (let change of changes) {
+        hljs.highlightBlock(change.target);
+        observer.unobserve(change.target);
+    }
+}, {});
+
 export function apply($el) {
     if (!Settings.get('w-highlight-code')) {
         return;
     }
-    $el.each(function(i, block) {
-        hljs.highlightBlock(block);
+
+    observedTargets.forEach(function (t) {
+        observer.unobserve(t);
+    });
+
+    $el.each(function (i, block) {
+        observer.observe(block);
+        observedTargets.push(block);
     });
 }
 

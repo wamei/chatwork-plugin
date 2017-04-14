9ed25a2e30dd014f2c24456f9359adb3e6ef78cb
wamei <wamei.cho@gmail.com>
2017-04-14 15:20:31 +0900

    do not check already updated message

--- src/chatwork/roomlist.js
+++ src/chatwork/roomlist.js
@@ -27,7 +27,10 @@ $(function () {
                 room.mention_num = 0;
                 continue;
             }
-            for(var e = chats.length-1, l = e; l > e - unread; l--){
+            let checked_index = room.timeline.checked || 0;
+            let unread_index = chats.length - 1 - unread;
+            let start = Math.max(checked_index, unread_index);
+            for(var l = chats.length - 1; l > start; l--){
                 if(chats[l] == void 0){
                     continue;
                 }
@@ -48,6 +51,7 @@ $(function () {
                     continue;
                 }
             }
+            room.timeline.checked = chats.length - 1;
         }
         b.build();
     };
